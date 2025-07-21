129b8d942d4f8f88f11f19c4250412883aad9665
Henrique Balduino Gonzaga <henrique.gonzaga@cebraspe.org.br>
2025-07-21 14:55:24 -0300

    04-03 requisição de dados

--- src/Components/MovieSection/index.tsx
+++ src/Components/MovieSection/index.tsx
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Styles from "./MovieSection.module.css";
 import FieldSet from "../FieldSet";
 import InputText from "../InputText";
 import Button from "../Button";
 import { FaSearch } from "react-icons/fa";
 import MovieList from "../MovieList";
-import { movies } from "../../Mock/data";
+import type { Movie } from "../../Types";
+import { getMovies } from "../../api";
 
 const MovieSection = () => {
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const fetchMovies = async () => {
+    try {
+      const movies = await getMovies();
+      setMovies(movies);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchMovies();
+  });
+
   return (
     <main>
       <section className={Styles.container}>
